f27d0c0cd092007c5eb2eb9b0c98c0bfb3e0c275
ParkerGits <parkerjlandon@gmail.com>
2023-12-26 23:07:13 -0800

    chore: format main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
-import           Data.Maybe
-import           Interpreter
-import           IO          (readHSON, readJSON, run)
-import           JSONParser  (decode)
-import           Opts        (HSONInput (CmdLineIn, HSONFileInput),
-                              Options (Options, hsonInput, jsonInput), testOpts)
+import Data.Maybe
+import IO (readHSON, readJSON, run)
+import Interpreter
+import JSONParser (decode)
+import Opts (
+  HSONInput (CmdLineIn, HSONFileInput),
+  Options (Options, hsonInput, jsonInput),
+  testOpts,
+ )
 
 main = do
   opts <- testOpts
