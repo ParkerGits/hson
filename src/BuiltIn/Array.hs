8af446077ecd9bc55a07f432f5734af5133b7ead
ParkerGits <parkerjlandon@gmail.com>
2024-01-04 17:38:47 -0800

    refactor: delete general methods, start making methods syntax sugar for functions

--- src/BuiltIn/Array.hs
+++ src/BuiltIn/Array.hs
@@ -2,6 +2,7 @@
 
 module BuiltIn.Array where
 
+import BuiltIn.Function
 import BuiltIn.General
 import BuiltIn.Helpers
 import Control.Monad.Except
@@ -18,67 +19,90 @@ import HSONValue
 arrayMethods :: Map.Map T.Text HSONValue
 arrayMethods =
   Map.fromList
-    [ ("length", mkMethod hsonLength)
-    , ("at", mkMethod hsonAt)
-    , ("map", mkMethod hsonMap)
-    , ("filter", mkMethod hsonFilter)
-    , ("reduce", mkMethod hsonReduce)
-    , ("every", mkMethod hsonEvery)
-    , ("some", mkMethod hsonSome)
-    , ("find", mkMethod hsonFind)
-    , ("sort", mkMethod hsonSort)
-    , ("insert", mkMethod hsonInsert)
-    , ("splice", mkMethod hsonSplice)
-    , ("push", mkMethod hsonPush)
-    , ("unshift", mkMethod hsonUnshift)
-    , ("pop", mkMethod hsonPop)
-    , ("shift", mkMethod hsonShift)
-    , ("with", mkMethod hsonWith)
-    , ("join", mkMethod hsonJoin)
-    , ("reverse", mkMethod hsonReverse)
-    , ("toString", mkMethod hsonToString)
-    , ("toJSON", mkMethod hsonToJSON)
+    [ ("length", mkMethod arrayLength)
+    , ("at", mkMethod arrayAt)
+    , ("map", mkMethod arrayMap)
+    , ("filter", mkMethod arrayFilter)
+    , ("reduce", mkMethod arrayReduce)
+    , ("every", mkMethod arrayEvery)
+    , ("some", mkMethod arraySome)
+    , ("find", mkMethod arrayFind)
+    , ("sort", mkMethod arraySort)
+    , ("insert", mkMethod arrayInsert)
+    , ("splice", mkMethod arraySplice)
+    , ("push", mkMethod arrayPush)
+    , ("unshift", mkMethod arrayUnshift)
+    , ("pop", mkMethod arrayPop)
+    , ("shift", mkMethod arrayShift)
+    , ("with", mkMethod arrayWith)
+    , ("join", mkMethod arrayJoin)
+    , ("reverse", mkMethod arrayReverse)
+    , ("toString", mkMethod arrayToString)
+    , ("toJSON", mkMethod arrayToJSON)
     ]
 
-hsonMap :: MethodDefinition
-hsonMap (Array arr) [Lambda (Func f) env] = Array <$> local (const env) (V.mapM (f . singleton) arr)
-hsonMap _ [arg] = throwError $ UnexpectedType "lambda" (showType arg)
-hsonMap _ args = throwError $ ArgumentCount 1 args
+arrayLength :: MethodDefinition
+arrayLength this [] = hsonLength [this]
+arrayLength _ args = throwError $ ArgumentCount 0 args
 
-hsonFilter :: MethodDefinition
-hsonFilter (Array arr) [Lambda f env] = Array <$> local (const env) (V.filterM (returnsTruthy f) arr)
-hsonFilter _ [arg] = throwError $ UnexpectedType "lambda" (showType arg)
-hsonFilter _ args = throwError $ ArgumentCount 1 args
+arrayToString :: MethodDefinition
+arrayToString this [] = hsonToString [this]
+arrayToString _ args = throwError $ ArgumentCount 0 args
 
-hsonReduce :: MethodDefinition
-hsonReduce (Array arr) [Lambda (Func f) env, initial] = local (const env) (V.foldM (\a b -> f [a, b]) initial arr)
-hsonReduce (Array arr) [arg, _] = throwError $ UnexpectedType "lambda" (showType arg)
-hsonReduce _ args = throwError $ ArgumentCount 2 args
+arrayToJSON :: MethodDefinition
+arrayToJSON this [Number n] = hsonToJSON [this, Number n]
+arrayToJSON this [] = hsonToJSON [this]
+arrayToJSON _ [arg] = throwError $ UnexpectedType "number" (showType arg)
+arrayToJSON _ args = throwError $ VariadicArgCount 0 1 args
 
-hsonEvery :: MethodDefinition
-hsonEvery (Array arr) [Lambda f env] = local (const env) (Bool <$> vAllM (returnsTruthy f) arr)
-hsonEvery (Array arr) [arg] = throwError $ UnexpectedType "lambda" (showType arg)
-hsonEvery _ args = throwError $ ArgumentCount 1 args
+arrayAt :: MethodDefinition
+arrayAt this [Number n] = hsonToJSON [this, Number n]
+arrayAt _ [arg] = throwError $ UnexpectedType "number" (showType arg)
+arrayAt _ args = throwError $ ArgumentCount 1 args
 
-hsonSome :: MethodDefinition
-hsonSome (Array arr) [Lambda f env] = local (const env) (Bool <$> vAnyM (returnsTruthy f) arr)
-hsonSome (Array arr) [arg] = throwError $ UnexpectedType "lambda" (showType arg)
-hsonSome _ args = throwError $ ArgumentCount 1 args
+arrayReverse :: MethodDefinition
+arrayReverse this [] = hsonToJSON [this]
+arrayReverse _ args = throwError $ ArgumentCount 0 args
 
-hsonFind :: MethodDefinition
-hsonFind (Array arr) [Lambda f env] = local (const env) (fromMaybe Null <$> vFindM (returnsTruthy f) arr)
-hsonFind (Array arr) [arg] = throwError $ UnexpectedType "lambda" (showType arg)
-hsonFind _ args = throwError $ ArgumentCount 1 args
+arrayMap :: MethodDefinition
+arrayMap (Array arr) [Lambda (Func f) env] = Array <$> local (const env) (V.mapM (f . singleton) arr)
+arrayMap _ [arg] = throwError $ UnexpectedType "lambda" (showType arg)
+arrayMap _ args = throwError $ ArgumentCount 1 args
 
-hsonSort :: MethodDefinition
-hsonSort (Array arr) [] = return $ Array $ V.map fromSorted $ MV.modify VA.sort (V.map Asc arr)
-hsonSort (Array arr) [String s] = case s of
+arrayFilter :: MethodDefinition
+arrayFilter (Array arr) [Lambda f env] = Array <$> local (const env) (V.filterM (returnsTruthy f) arr)
+arrayFilter _ [arg] = throwError $ UnexpectedType "lambda" (showType arg)
+arrayFilter _ args = throwError $ ArgumentCount 1 args
+
+arrayReduce :: MethodDefinition
+arrayReduce (Array arr) [Lambda (Func f) env, initial] = local (const env) (V.foldM (\a b -> f [a, b]) initial arr)
+arrayReduce (Array arr) [arg, _] = throwError $ UnexpectedType "lambda" (showType arg)
+arrayReduce _ args = throwError $ ArgumentCount 2 args
+
+arrayEvery :: MethodDefinition
+arrayEvery (Array arr) [Lambda f env] = local (const env) (Bool <$> vAllM (returnsTruthy f) arr)
+arrayEvery (Array arr) [arg] = throwError $ UnexpectedType "lambda" (showType arg)
+arrayEvery _ args = throwError $ ArgumentCount 1 args
+
+arraySome :: MethodDefinition
+arraySome (Array arr) [Lambda f env] = local (const env) (Bool <$> vAnyM (returnsTruthy f) arr)
+arraySome (Array arr) [arg] = throwError $ UnexpectedType "lambda" (showType arg)
+arraySome _ args = throwError $ ArgumentCount 1 args
+
+arrayFind :: MethodDefinition
+arrayFind (Array arr) [Lambda f env] = local (const env) (fromMaybe Null <$> vFindM (returnsTruthy f) arr)
+arrayFind (Array arr) [arg] = throwError $ UnexpectedType "lambda" (showType arg)
+arrayFind _ args = throwError $ ArgumentCount 1 args
+
+arraySort :: MethodDefinition
+arraySort (Array arr) [] = return $ Array $ V.map fromSorted $ MV.modify VA.sort (V.map Asc arr)
+arraySort (Array arr) [String s] = case s of
   "asc" -> return $ Array $ V.map fromSorted $ MV.modify VA.sort (V.map Asc arr)
   "dsc" -> return $ Array $ V.map fromSorted $ MV.modify VA.sort (V.map Desc arr)
   arg ->
     throwError $ UnexpectedType "\"asc\" or \"dsc\"" (T.concat ["\"", arg, "\""])
-hsonSort _ [arg] = throwError $ UnexpectedType "string" (showType arg)
-hsonSort (Array arr) [String s, String key] = case s of
+arraySort _ [arg] = throwError $ UnexpectedType "string" (showType arg)
+arraySort (Array arr) [String s, String key] = case s of
   "abk" ->
     return $ Array $ V.map fromSorted $ MV.modify VA.sort (V.map (AscByKey key) arr)
   "dbk" ->
@@ -88,62 +112,62 @@ hsonSort (Array arr) [String s, String key] = case s of
           MV.modify VA.sort (V.map (DescByKey key) arr)
   arg ->
     throwError $ UnexpectedType "\"abk\" or \"dbk\"" (T.concat ["\"", arg, "\""])
-hsonSort _ [arg, String _] = throwError $ UnexpectedType "string" (showType arg)
-hsonSort _ [_, arg] = throwError $ UnexpectedType "string" (showType arg)
-hsonSort _ args = throwError $ VariadicArgCount 0 2 args
+arraySort _ [arg, String _] = throwError $ UnexpectedType "string" (showType arg)
+arraySort _ [_, arg] = throwError $ UnexpectedType "string" (showType arg)
+arraySort _ args = throwError $ VariadicArgCount 0 2 args
 
-hsonInsert :: MethodDefinition
-hsonInsert (Array arr) [Number n, v] = do
+arrayInsert :: MethodDefinition
+arrayInsert (Array arr) [Number n, v] = do
   index <- indexFromNumber n (Array arr)
   let (x, y) = V.splitAt index arr
    in return $ Array $ x <> V.singleton v <> y
-hsonInsert (Array arr) [arg, _] = throwError $ UnexpectedType "integer" (showType arg)
-hsonInsert _ args = throwError $ ArgumentCount 2 args
+arrayInsert (Array arr) [arg, _] = throwError $ UnexpectedType "integer" (showType arg)
+arrayInsert _ args = throwError $ ArgumentCount 2 args
 
-hsonSplice :: MethodDefinition
-hsonSplice (Array arr) [Number n] = do
+arraySplice :: MethodDefinition
+arraySplice (Array arr) [Number n] = do
   index <- indexFromNumber n (Array arr)
   return $ Array $ V.take index arr
-hsonSplice (Array arr) [Number idx, Number n] = do
+arraySplice (Array arr) [Number idx, Number n] = do
   index <- indexFromNumber idx (Array arr)
   dropCount <- intFromNumber n
   let (x, y) = V.splitAt index arr
    in return $ Array $ x <> V.drop dropCount y
-hsonSplice (Array arr) [Number idx, Number n, Array inserts] = do
+arraySplice (Array arr) [Number idx, Number n, Array inserts] = do
   index <- indexFromNumber idx (Array arr)
   dropCount <- intFromNumber n
   let (x, y) = V.splitAt index arr
    in return $ Array $ x <> inserts <> V.drop dropCount y
-hsonSplice (Array arr) (Number _ : Number _ : arg : _) = throwError $ UnexpectedType "array" (showType arg)
-hsonSplice (Array arr) (Number _ : arg : _) = throwError $ UnexpectedType "integer" (showType arg)
-hsonSplice (Array arr) (arg : _) = throwError $ UnexpectedType "integer" (showType arg)
-hsonSplice _ args = throwError $ VariadicArgCount 1 3 args
+arraySplice (Array arr) (Number _ : Number _ : arg : _) = throwError $ UnexpectedType "array" (showType arg)
+arraySplice (Array arr) (Number _ : arg : _) = throwError $ UnexpectedType "integer" (showType arg)
+arraySplice (Array arr) (arg : _) = throwError $ UnexpectedType "integer" (showType arg)
+arraySplice _ args = throwError $ VariadicArgCount 1 3 args
 
-hsonWith :: MethodDefinition
-hsonWith (Array arr) [Number n, v] = do
+arrayWith :: MethodDefinition
+arrayWith (Array arr) [Number n, v] = do
   index <- indexFromNumber n (Array arr)
   if index `inBounds` arr
     then return $ Array $ arr V.// [(index, v)]
     else throwError $ IndexOutOfBounds' index
-hsonWith (Array arr) [arg, _] = throwError $ UnexpectedType "integer" (showType arg)
-hsonWith _ args = throwError $ ArgumentCount 2 args
+arrayWith (Array arr) [arg, _] = throwError $ UnexpectedType "integer" (showType arg)
+arrayWith _ args = throwError $ ArgumentCount 2 args
 
-hsonJoin :: MethodDefinition
-hsonJoin (Array arr) [String s] = return $ String $ T.concat $ intersperse s $ V.toList $ V.map showValue arr
-hsonJoin _ args = throwError $ ArgumentCount 1 args
+arrayJoin :: MethodDefinition
+arrayJoin (Array arr) [String s] = return $ String $ T.concat $ intersperse s $ V.toList $ V.map showValue arr
+arrayJoin _ args = throwError $ ArgumentCount 1 args
 
-hsonPush :: MethodDefinition
-hsonPush (Array arr) [v] = return $ Array $ V.snoc arr v
-hsonPush _ args = throwError $ ArgumentCount 1 args
+arrayPush :: MethodDefinition
+arrayPush (Array arr) [v] = return $ Array $ V.snoc arr v
+arrayPush _ args = throwError $ ArgumentCount 1 args
 
-hsonUnshift :: MethodDefinition
-hsonUnshift (Array arr) [v] = return $ Array $ V.cons v arr
-hsonUnshift _ args = throwError $ ArgumentCount 1 args
+arrayUnshift :: MethodDefinition
+arrayUnshift (Array arr) [v] = return $ Array $ V.cons v arr
+arrayUnshift _ args = throwError $ ArgumentCount 1 args
 
-hsonPop :: MethodDefinition
-hsonPop (Array arr) [] = return $ Array $ V.init arr
-hsonPop _ args = throwError $ ArgumentCount 0 args
+arrayPop :: MethodDefinition
+arrayPop (Array arr) [] = return $ Array $ V.init arr
+arrayPop _ args = throwError $ ArgumentCount 0 args
 
-hsonShift :: MethodDefinition
-hsonShift (Array arr) [] = return $ Array $ V.tail arr
-hsonShift _ args = throwError $ ArgumentCount 0 args
+arrayShift :: MethodDefinition
+arrayShift (Array arr) [] = return $ Array $ V.tail arr
+arrayShift _ args = throwError $ ArgumentCount 0 args
--- src/BuiltIn/Function.hs
+++ src/BuiltIn/Function.hs
@@ -3,6 +3,8 @@
 module BuiltIn.Function where
 
 import BuiltIn.Helpers
+import Control.Exception
+import Control.Monad.Cont (MonadIO (liftIO))
 import Control.Monad.Error.Class
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Encode.Pretty as AP
@@ -20,7 +22,7 @@ builtInFunctions =
     [ ("keys", mkFunction keys)
     , ("values", mkFunction values)
     , ("hasProperty", mkFunction hasProperty)
-    , ("toJSON", mkFunction toJSON)
+    , ("toJSON", mkFunction hsonToJSON)
     ]
 
 keys :: FunctionDefinition
@@ -39,8 +41,8 @@ hasProperty [Object _, arg] = throwError $ UnexpectedType "string" (showType arg
 hasProperty [arg, _] = throwError $ UnexpectedType "object" (showType arg)
 hasProperty args = throwError $ ArgumentCount 2 args
 
-toJSON :: FunctionDefinition
-toJSON [arg, Number n] = do
+hsonToJSON :: FunctionDefinition
+hsonToJSON [arg, Number n] = do
   spaces <- intFromNumber n
   case T.decodeUtf8' $
     AP.encodePretty'
@@ -48,8 +50,41 @@ toJSON [arg, Number n] = do
       arg of
     Left err -> throwError $ JSONEncodingError err
     Right t -> return $ String $ T.toStrict t
-toJSON [_, arg] = throwError $ UnexpectedType "number" (showType arg)
-toJSON [arg] = case T.decodeUtf8' $ A.encode arg of
+hsonToJSON [_, arg] = throwError $ UnexpectedType "number" (showType arg)
+hsonToJSON [arg] = case T.decodeUtf8' $ A.encode arg of
   Left err -> throwError $ JSONEncodingError err
   Right t -> return $ String $ T.toStrict t
-toJSON args = throwError $ VariadicArgCount 1 2 args
+hsonToJSON args = throwError $ VariadicArgCount 1 2 args
+
+hsonToString :: FunctionDefinition
+hsonToString [arg] = return $ String $ showValue arg
+hsonToString args = throwError $ ArgumentCount 1 args
+
+hsonLength :: FunctionDefinition
+hsonLength [Array arr] = return $ Number $ fromIntegral $ V.length arr
+hsonLength [String str] = return $ Number $ fromIntegral $ T.length str
+hsonLength [arg] = throwError $ UnexpectedType "array or string" (showType arg)
+hsonLength args = throwError $ ArgumentCount 1 args
+
+-- Returns the value at the index n. If n is negative, the checked index is index + n. If n is out of bounds, returns Null.
+hsonAt :: FunctionDefinition
+hsonAt [arg, Number n] = do
+  idx <- indexFromNumber n arg
+  case arg of
+    Array arr -> case arr V.!? idx of
+      Just v -> return v
+      Nothing -> return Null
+    String str -> do
+      result <- liftIO $ try $ evaluate $ T.index str idx
+      case result :: Either SomeException Char of
+        Right result -> return $ String $ T.singleton result
+        Left _ -> return Null
+    _ -> throwError $ UnexpectedType "array or string" (showType arg)
+hsonAt [_, arg] = throwError $ UnexpectedType "number" (showType arg)
+hsonAt args = throwError $ ArgumentCount 2 args
+
+hsonReverse :: FunctionDefinition
+hsonReverse [Array arr] = return $ Array $ V.reverse arr
+hsonReverse [String str] = return $ String $ T.reverse str
+hsonReverse [arg] = throwError $ UnexpectedType "array or string" (showType arg)
+hsonReverse args = throwError $ ArgumentCount 1 args
